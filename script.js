b02ce35b4ca740a84b98295b42d71ba1e174c3a9
Yu Ting Tseng <r12725004@ntu.edu.tw>
2024-03-09 22:13:18 +0800

    Add blank project resume & adjust animation

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
-document.getElementById('Neon').addEventListener('click', function() {
-    var animatedDiv = document.getElementById('Neon');
-    animatedDiv.classList.add('animate');
+var aboutmeButtons = document.getElementsByClassName('aboutme_button');
+var showprojectButtons = document.getElementsByClassName('project_button');
+var experienceButtons = document.getElementsByClassName('experience_button');
+
+var projectDiv = document.getElementById('project');
+var experienceDiv = document.getElementById('experience');
+var animated = false;
+
+
+document.getElementById('experience').addEventListener('click', function experiencehandleClick() {
+    if(!animated){
+        experienceDiv.classList.add('animate');
+        projectDiv.style.display = 'none';
+        document.getElementById('experience').removeEventListener('click', experiencehandleClick);
+        console.log("experience animate");
+        animated = true;    
+    }
 });
+
+document.getElementById('project').addEventListener('click', function projecthandleClick() {
+    if(!animated){
+        project.classList.add('animate');
+        experienceDiv.style.display = 'none';
+        document.getElementById('project').removeEventListener('click', projecthandleClick);
+        console.log("project animate");
+        animated = true    
+    }
+});
+
+
+for (let button of showprojectButtons) {
+    button.addEventListener('click', function() {
+        experienceDiv.style.display = 'none';
+
+        projectDiv.classList.remove('animate');
+        projectDiv.style.display = 'block';
+        projectDiv.style.transform = 'translate(0%, 0%) rotate(0)';
+        
+        console.log("Show project")
+    });
+}
+
+for (let button of experienceButtons) {
+    button.addEventListener('click', function() {
+        projectDiv.style.display = 'none';
+
+        experienceDiv.classList.remove('animate');
+        experienceDiv.style.display = 'block';
+        experienceDiv.style.transform = 'translate(0%, 0%) rotate(0)';
+
+        console.log("Show experience")
+    });
+}
